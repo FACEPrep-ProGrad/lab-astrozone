a39c2caba03a257187290bf3555f103506bc493e
Varsha <hp@DESKTOP-4IENAI5>
2020-05-13 10:39:32 +0530

    commit

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,48 @@
 package model;
 
-//Type your code here
+import java.time.LocalDate;
+
+public class User
+{
+	private String name;
+	private String email;
+	public LocalDate dob;
+	private String gender;
+
+	
+	public User(String name,String email,String dob,String gender)
+	{
+		setName(name);
+		setEmail(email);
+		this.dob=LocalDate.parse(dob);
+		this.gender=gender;
+	}
+	
+	public void getName()
+	{
+		System.out.println(name);
+	}
+
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	
+	public void getEmail()
+	{
+		System.out.println(email);
+	}
+	
+	public void setEmail(String email)
+	{
+		this.email=email;
+	}
+	
+	public void setgender(String gender) {
+		this.gender=gender;
+	}
+	public void getgender() {
+		System.out.println(gender);
+	}
+	
+}
--- src/service/AstroCalculator.java
+++ src/service/AstroCalculator.java
@@ -1,3 +1,101 @@
 package service;
+import model.User;
+import java.time.*;
 
-// Type your code
+public class AstroCalculator
+{
+	
+	
+	public String findSign(User us)
+	
+	{
+		Month mon=us.dob.getMonth();
+		int month=mon.getValue();
+		int dt=us.dob.getDayOfMonth();
+		String zodiac=" ";
+		
+		if (month == 1){ 
+            if (dt < 20) 
+         zodiac = "Capricorn"; 
+            else
+            zodiac = "aquarius"; 
+        } 
+              
+        else if (month == 2){ 
+            if (dt < 19) 
+            zodiac = "Aquarius"; 
+            else
+            zodiac = "pisces"; 
+        } 
+              
+        else if(month == 3){ 
+            if (dt < 21)  
+            zodiac = "Pisces"; 
+            else
+           zodiac = "aries"; 
+        } 
+        else if (month == 4){ 
+            if (dt < 20) 
+            zodiac = "Aries"; 
+            else
+            zodiac = "taurus"; 
+        } 
+              
+        else if (month == 5){ 
+            if (dt < 21) 
+            zodiac = "Taurus"; 
+            else
+            zodiac = "gemini"; 
+        } 
+              
+        else if( month == 6){ 
+            if (dt < 21) 
+            zodiac = "Gemini"; 
+            else
+            zodiac = "cancer"; 
+        } 
+              
+        else if (month == 7){ 
+            if (dt < 23) 
+            zodiac = "Cancer"; 
+            else
+            zodiac = "leo"; 
+        } 
+              
+        else if( month == 8){ 
+            if (dt < 23)  
+            zodiac = "Leo"; 
+            else
+           zodiac = "virgo"; 
+        } 
+              
+        else if (month == 9){ 
+            if (dt < 23) 
+            zodiac = "Virgo"; 
+            else
+           zodiac = "libra"; 
+        } 
+              
+        else if (month == 10){ 
+            if (dt < 23) 
+            zodiac = "Libra"; 
+            else
+          zodiac= "scorpio"; 
+        } 
+              
+        else if (month == 11){ 
+            if (dt < 22) 
+           zodiac = "scorpio"; 
+            else
+            zodiac = "sagittarius"; 
+        }
+        else  if (month == 12){ 
+            if (dt < 22) 
+            zodiac = "Sagittarius"; 
+            else
+            zodiac ="capricorn"; 
+        } 
+		return zodiac;
+		
+	}
+}
