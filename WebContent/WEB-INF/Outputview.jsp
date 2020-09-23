ec4a5dfefa471a91c0dc010457ea5486fc7b4c4c
vamshiduddu <44239287+vamshiduddu@users.noreply.github.com>
2020-09-23 15:40:11 +0530

    1372364

--- WebContent/WEB-INF/Outputview.jsp
+++ WebContent/WEB-INF/Outputview.jsp
@@ -3,13 +3,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+body {
+  background-color: black;
+}
+</style>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
  <link rel="stylesheet" href="style.css">
 </head>
+<div >
+<h1 style="color:gold;text-align:center; font-size:14dp">Your details</h1>
+ 
+<p style="text-align:center;"><b>Name:</b>
+ <%= request.getParameter("name")%>
+</p>
+<p style="text-align:center;"><b>Age:</b>
+ <%= request.getParameter("name")%>
+</p>
+<p style="text-align:center;"><b>gender:</b>
+ <%= request.getParameter("gender")%>
+</p>
+
+</div>
 <body>
 <div id="zodiac">
+
 ${astro}
 </div>
+
+
 </body>
 </html>
--- src/service/AstroCalculator.java
+++ src/service/AstroCalculator.java
@@ -1,3 +1,133 @@
 package service;
 
+import model.User;
+
 // Type your code
+public class AstroCalculator{
+	
+	public  String findSign(User user) {
+		
+		String[] dob = user.getDob().split("-");
+		
+		int day = Integer.parseInt(dob[0]);
+		int month = Integer.parseInt(dob[1]);
+		int year = Integer.parseInt(dob[2]);
+		String sign="";
+		
+		switch (month) {
+		case 1:
+			if (day < 20)
+	            sign = "Capricorn";
+	         else
+	            sign = "Aquarius";
+			break;
+		
+
+		case 2:
+			if (day < 19)
+	            sign = "Aquarius";
+	         else
+	            sign = "Pisces";
+			
+			break;
+		
+
+		case 3:
+			if (day < 21)
+	            sign = "Pisces";
+	         else
+	            sign = "Aries";
+			
+			break;
+		
+
+		case 4:
+			 if (day < 20)
+		            sign = "Aries";
+		         else
+		            sign = "Taurus";
+			
+			break;
+		
+
+		case 5:
+			 if (day < 21)
+		            sign = "Taurus";
+		         else
+		            sign = "Gemini";
+			
+			break;
+		
+
+		case 6:
+			if (day < 21)
+	            sign = "Gemini";
+	         else
+	            sign = "Cancer";
+			
+			break;
+		
+
+		case 7:
+			 if (day < 23)
+		            sign = "Cancer";
+		         else
+		            sign = "Leo";
+			break;
+		
+
+		case 8:
+			 if (day < 23)
+		            sign = "Leo";
+		         else
+		            sign = "Virgo";
+			
+			break;
+		
+
+		case 9:
+			   if (day < 23)
+		            sign = "Virgo";
+		         else
+		            sign = "Libra";
+			
+			break;
+		
+
+		case 10:
+			 if (day < 23)
+		            sign = "Libra";
+		         else
+		            sign = "Scorpio";
+			
+			break;
+		
+
+		case 11:
+			if(day <= 22)
+				sign =  "Scorpio";
+			else if(day >= 23)
+				sign =  "Sagittarius";
+			break;
+			
+			
+		
+
+		case 12:
+			if (day < 22)
+	            sign = "Sagittarius";
+	         else
+	            sign ="Capricorn";
+			
+			break;
+		
+
+		default:
+			break;
+		}
+		
+		
+		return sign;
+		
+	}
+}
